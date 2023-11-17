38f9c48af11ce97eb827d3f27669ed8f3b308161
branow <bodnarorestdraw@gmail.com>
2023-11-17 11:36:33 +0200

    init commit

---
+++ src/main/java/com/branow/nppaccountingsystem/NppAccountingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.branow.nppaccountingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NppAccountingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NppAccountingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/branow/nppaccountingsystem/NppAccountingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.branow.nppaccountingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NppAccountingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
